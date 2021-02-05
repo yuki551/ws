ac39d672e3866bd4baa2af89a012491897bd30fe
yuki551 <nomurayuki.sozosha@gmail.com>
2021-02-05 16:34:39 +0900

    ボタン文言修正

--- backend/resources/js/Pages/User/Create.vue
+++ backend/resources/js/Pages/User/Create.vue
@@ -134,7 +134,7 @@
                                     class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5"
                                     v-show="!editMode"
                                     @click="confirm(form)">
-                                    登録
+                                    確認
                                 </button>
                             </span>
                         </div>
