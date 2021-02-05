aac15936756f91328592502c90324a65e42df43a
yuki551 <nomurayuki.sozosha@gmail.com>
2021-02-05 14:20:40 +0900

    タイトル

--- backend/resources/js/Pages/clientlists.vue
+++ backend/resources/js/Pages/clientlists.vue
@@ -2,7 +2,7 @@
     <app-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
+                取引先一覧
             </h2>
         </template>
         <div class="py-12">
