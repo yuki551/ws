b875f8c027935fb1e3353cfd3327c5db3b39e11d
yuki551 <nomurayuki.sozosha@gmail.com>
2021-02-08 16:34:41 +0900

    レスポンシブ途中

--- backend/resources/js/Layouts/AppLayout.vue
+++ backend/resources/js/Layouts/AppLayout.vue
@@ -168,12 +168,6 @@
 
             <!-- Responsive Navigation Menu -->
             <div :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }" class="sm:hidden">
-                <div class="pt-2 pb-3 space-y-1">
-                    <jet-responsive-nav-link href="/dashboard" :active="route().current('dashboard.index')">
-                        Dashboard
-                    </jet-responsive-nav-link>
-                </div>
-
                 <!-- Responsive Settings Options -->
                 <div class="pt-4 pb-1 border-t border-gray-200">
                     <div class="flex items-center px-4">
@@ -188,17 +182,55 @@
                     </div>
 
                     <div class="mt-3 space-y-1">
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            Account
+                        </div>
+
                         <jet-responsive-nav-link :href="route('profile.show')" :active="route().current('profile.show')">
-                            Profile
+                            プロフィール
                         </jet-responsive-nav-link>
 
-                        <jet-responsive-nav-link
-                            :href="route('api-tokens.index')"
-                            :active="route().current('api-tokens.index')"
-                            v-if="$page.jetstream.hasApiFeatures"
-                        >
-                            API Tokens
+                        <div class="border-t border-gray-200"></div>
+
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            Post
+                        </div>
+
+                        <jet-responsive-nav-link :href="route('post.create')" v-if="$page.user.role_id != 3">
+                            日報登録
                         </jet-responsive-nav-link>
+
+                        <jet-responsive-nav-link :href="route('post.index')">
+                            日報一覧
+                        </jet-responsive-nav-link>
+
+                        <jet-responsive-nav-link :href="route('mypostlists.index')">
+                            My日報一覧
+                        </jet-responsive-nav-link>
+
+                        <div class="border-t border-gray-200"></div>
+
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            Management
+                        </div>
+
+                        <jet-responsive-nav-link :href="route('user.create')" v-if="$page.user.auth_id == 1">
+                            社員登録
+                        </jet-responsive-nav-link>
+
+                        <jet-responsive-nav-link :href="route('user.index')">
+                            社員一覧
+                        </jet-responsive-nav-link>
+
+                        <jet-responsive-nav-link :href="route('clients_create.index')" v-if="$page.user.current_team_id == 3 || $page.user.current_team_id == 4">
+                            取引先登録
+                        </jet-responsive-nav-link>
+
+                        <jet-responsive-nav-link :href="route('clientlists.index')">
+                            取引先一覧
+                        </jet-responsive-nav-link>
+
+                        <div class="border-t border-gray-200"></div>
 
                         <!-- Authentication -->
                         <form method="POST" @submit.prevent="logout">
@@ -207,55 +239,6 @@
                             </jet-responsive-nav-link>
                         </form>
 
-                        <!-- Team Management -->
-                        <template v-if="$page.jetstream.hasTeamFeatures">
-                            <div class="border-t border-gray-200"></div>
-
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                Manage Team
-                            </div>
-
-                            <!-- Team Settings -->
-                            <jet-responsive-nav-link
-                                :href="route('teams.show', $page.user.current_team)"
-                                :active="route().current('teams.show')"
-                            >
-                                Team Settings
-                            </jet-responsive-nav-link>
-
-                            <jet-responsive-nav-link :href="route('teams.create')" :active="route().current('teams.create')">
-                                Create New Team
-                            </jet-responsive-nav-link>
-
-                            <div class="border-t border-gray-200"></div>
-
-                            <!-- Team Switcher -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                Switch Teams
-                            </div>
-
-                            <template v-for="team in $page.user.all_teams">
-                                <form @submit.prevent="switchToTeam(team)" :key="team.id">
-                                    <jet-responsive-nav-link as="button">
-                                        <div class="flex items-center">
-                                            <svg
-                                                v-if="team.id == $page.user.current_team_id"
-                                                class="mr-2 h-5 w-5 text-green-400"
-                                                fill="none"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                stroke-width="2"
-                                                stroke="currentColor"
-                                                viewBox="0 0 24 24"
-                                            >
-                                                <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                            </svg>
-                                            <div>{{ team.name }}</div>
-                                        </div>
-                                    </jet-responsive-nav-link>
-                                </form>
-                            </template>
-                        </template>
                     </div>
                 </div>
             </div>
--- backend/resources/js/Pages/Post/Index.vue
+++ backend/resources/js/Pages/Post/Index.vue
@@ -5,7 +5,7 @@
                 日報一覧
             </h2>
         </template>
-        <div class="py-12">
+        <div class="py-12 page-postList">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-7 py-4">
                     <div
